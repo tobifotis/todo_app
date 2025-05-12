f2cbf138d45d54c1fd2d68270e64cf6c190910ff
Emmanuel, Oluwatobi <oluwatobiemmanuel@my.unt.edu>
2025-05-11 19:38:30 -0500

    feat: add edit feature to update existing todos by index

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 todos = []
 
 while True:
-    user_action = input("Type add, show, or exit: ")
+    user_action = input("Type add, show, edit, or exit: ")
     user_action = user_action.strip()
 
     match user_action:
@@ -10,8 +10,11 @@ while True:
             todos.append(todo)
         case 'show':
             for item in todos:
-                item = item.title()
                 print(item)
+        case 'edit':
+            number = int(input("Number of the todo to edit: "))
+            number = number - 1
+            new_todo = input("Enter new todo: ")
+            todos[number] = new_todo
         case "exit":
             break
-
