e16f213c85bae3513e7997094ba83684050f859d
Emmanuel, Oluwatobi <oluwatobiemmanuel@my.unt.edu>
2025-05-12 22:11:57 -0500

    feat: add complete command and numbered display for todos

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 todos = []
 
 while True:
-    user_action = input("Type add, show, edit, or exit: ")
+    user_action = input("Type add, show, edit, complete or exit: ")
     user_action = user_action.strip()
 
     match user_action:
@@ -9,12 +9,16 @@ while True:
             todo = input("Enter a todo: ")
             todos.append(todo)
         case 'show':
-            for item in todos:
-                print(item)
+            for index, item in enumerate(todos):
+                row = f"{index + 1}-{item}"
+                print(row)
         case 'edit':
             number = int(input("Number of the todo to edit: "))
             number = number - 1
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo
+        case 'complete':
+            number = int(input("Number of the todo to complete: "))
+            todos.pop(number - 1)
         case "exit":
             break
