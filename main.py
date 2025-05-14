7dc31eb0308e5721a3f545965bdfbff631d9b724
Emmanuel, Oluwatobi <oluwatobiemmanuel@my.unt.edu>
2025-05-14 08:36:57 -0500

    feat: add file persistence for todos using todos.txt

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
-todos = []
-
 while True:
     user_action = input("Type add, show, edit, complete or exit: ")
     user_action = user_action.strip()
 
     match user_action:
         case 'add':
-            todo = input("Enter a todo: ")
+            todo = input("Enter a todo: ") + "\n"
+
+            file = open("todos.txt", "r")
+            todos = file.readlines()
+            file.close()
+
             todos.append(todo)
+
+            file = open("todos.txt", "w")
+            file.writelines(todos)
+            file.close()
+
         case 'show':
+            file = open("todos.txt", "r")
+            todos = file.readlines()
+            file.close()
+
             for index, item in enumerate(todos):
                 row = f"{index + 1}-{item}"
                 print(row)
