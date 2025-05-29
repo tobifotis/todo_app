c592ff34dd43b7b87ea3108cf5259c4711ca271d
Emmanuel, Oluwatobi <oluwatobiemmanuel@my.unt.edu>
2025-05-29 10:12:46 -0500

    feat(gui): add complete and exit buttons with full todo lifecycle support

--- gui.py
+++ gui.py
@@ -9,9 +9,13 @@ list_box = sg.Listbox(values=functions.get_todos(), key="todos",
                       enable_events=True, size=[45, 10])
 
 edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
+
+layout=[[label], [input_box, add_button], [list_box, edit_button, complete_button], [exit_button]]
 
 window = sg.Window("My To-Do App",
-                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                   layout=layout,
                    font=("Helvetica", 15))
 
 while True:
@@ -43,11 +47,24 @@ while True:
 
             window["todos"].update(values=todos)
 
+        case "Complete":
+            todo_to_complete = values["todos"][0]
+
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+
+            window["todos"].update(values=todos)
+
+            window["todo"].update(value="")
+
+        case "Exit":
+            break
+
         case "todos":
             window["todo"].update(value=values["todos"][0])
 
         case sg.WIN_CLOSED:
             break
-
 
 window.close()
