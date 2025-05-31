bb2c960d5c0352f1b40e63a24809405be299797e
Emmanuel, Oluwatobi <oluwatobiemmanuel@my.unt.edu>
2025-05-30 22:18:26 -0500

    feat(web): create initial Streamlit UI with todo checkboxes and input field

---
+++ web.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import functions
+
+import os
+
+if not os.path.exists("todos.txt"):
+    with open("todos.txt", "w") as file:
+        pass
+
+todos = functions.get_todos()
+
+st.title("My Todo App")
+st.subheader("This is my todo app.")
+st.write("This app will help increase your productivity.")
+
+for todo in todos:
+    st.checkbox(todo)
+
+st.text_input(label="Enter a todo:", placeholder="Add new todo... ")
