9399d437db9cc03d18ad3d4c157d9894be62c85e
Emmanuel, Oluwatobi <oluwatobiemmanuel@my.unt.edu>
2025-05-30 22:39:35 -0500

    feat(web): implement live todo addition using Streamlit session state

--- web.py
+++ web.py
@@ -7,7 +7,14 @@ if not os.path.exists("todos.txt"):
     with open("todos.txt", "w") as file:
         pass
 
+
 todos = functions.get_todos()
+
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+
 
 st.title("My Todo App")
 st.subheader("This is my todo app.")
@@ -16,4 +23,5 @@ st.write("This app will help increase your productivity.")
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label="Enter a todo:", placeholder="Add new todo... ")
+st.text_input(label="Enter a todo:", placeholder="Add new todo... ",
+              on_change=add_todo, key="new_todo")
