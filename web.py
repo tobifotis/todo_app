283ab686a6daff4ff04cda9edf9d5b37d570ad24
Emmanuel, Oluwatobi <oluwatobiemmanuel@my.unt.edu>
2025-06-22 08:33:10 -0500

    feat: Create web interface with Streamlit

--- web.py
+++ web.py
@@ -20,8 +20,14 @@ st.title("My Todo App")
 st.subheader("This is my todo app.")
 st.write("This app will help increase your productivity.")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
+
 
 st.text_input(label="Enter a todo:", placeholder="Add new todo... ",
               on_change=add_todo, key="new_todo")
